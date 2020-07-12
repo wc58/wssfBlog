47d3693b59c0ca795ff69a5ba2d64d46161e9989
wc58 <2258354832@qq.com>
2020-07-12 15:00:45 +0800

    新增文章与标签相关联的mapper

---
+++ src/main/java/com/chao/wssf/mapper/ArticleLabelMapper.java
@@ -0,0 +1,11 @@
+package com.chao.wssf.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.chao.wssf.entity.ArticleLabel;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ArticleLabelMapper extends BaseMapper<ArticleLabel> {
+
+
+}
